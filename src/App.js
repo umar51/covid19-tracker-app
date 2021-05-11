1422722b2caaae03c3176daeadc277e29d819069
umar51 <66181738+umar51@users.noreply.github.com>
2021-05-12 01:02:53 +0500

    removed second index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, {useEffect, useState} from 'react';
 
 
-import {Cards, Chart, CountryPicker } from './components';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+
 import styles  from "./App.module.css";
 
 import fetchData from './api/api'; //{} is used as const fetchData was not exported as default 
